15211423b390d21000e68e85a77ce12d6033376a
hjpark625 <zero950@gmail.com>
2024-03-12 11:17:49 +0900

    feat: os 활용 데스크탑 path 자동 지정

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 import cv2
@@ -5,6 +6,7 @@ import numpy as np
 from tempfile import NamedTemporaryFile
 
 app = FastAPI()
+desktop_path = os.path.join(os.path.expanduser('~'), 'Desktop')
 
 @app.get('/')
 async def root():
@@ -22,26 +24,19 @@ async def process_image(image: UploadFile = File(...)):
     faces = face_cascade.detectMultiScale(img, 1.1, 4)
     for (x, y, w, h) in faces:
         img[y:y+h, x:x+w] = cv2.blur(img[y:y+h, x:x+w], (30, 30))
-        # cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 0), thickness=-1)
 
     # 번호판 인식 및 블러처리 얼굴 로직과 유사함
-    # TODO: 번호판 인식 모델이 러시아 번호판 모델인데 한국 번호판 모델이 따로 필요할지 생각해보기
     plate_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_russian_plate_number.xml')
     plates = plate_cascade.detectMultiScale(img, 1.1, 4)
     for (x, y, w, h) in plates:
         img[y:y+h, x:x+w] = cv2.blur(img[y:y+h, x:x+w], (30, 30))
-        # cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 0), thickness=-1)
-    
-    # 처리된 이미지 저장 및 변환
-    
-    # print(save_path)
 
     temp_file = NamedTemporaryFile(delete=True, suffix='.jpg')
     file_name = temp_file.name.split('/')[-1]
-    save_path = f'/Users/jeff/Desktop/{file_name}'
+    save_path = f'{desktop_path}/blurImages'
 
-    cv2.imwrite(save_path, img)
-    return FileResponse(path=save_path, media_type='image/jpeg')
+    cv2.imwrite(f'{save_path}/${file_name}', img)
+    return {'result': f'{save_path}에 저장됨'}
 
 @app.post('/images')
 async def multiple_images(images: list[UploadFile] = File(...)):
@@ -62,7 +57,7 @@ async def multiple_images(images: list[UploadFile] = File(...)):
         
         temp_file = NamedTemporaryFile(delete=True, suffix='.jpg')
         file_name = temp_file.name.split('/')[-1]
-        save_path = f'/Users/jeff/Desktop/{file_name}'
-        cv2.imwrite(save_path, img)
+        save_path = f'{desktop_path}/blurImages'
+        cv2.imwrite(f'{save_path}/{file_name}', img)
 
-    return FileResponse(path=save_path, media_type='image/jpeg')
+    return {'result': f'{save_path}에 저장됨'}
